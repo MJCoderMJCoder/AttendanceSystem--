b2617a637a4c6f4615c55ce55b7d8c2e00f2b27f
MJCoderMJCoder <598157378@qq.com>
2019-03-18 19:46:13 +0800

    安卓app考勤系统 人脸识别：（用虹软的离线sdk：             https://ai.arcsoft.com.cn/index.html             在线的face++的api：             https://www.faceplusplus.com.cn/）

--- app/src/main/java/com/lzf/attendancesystem/activity/AttendanceDataExportActivity.java
+++ app/src/main/java/com/lzf/attendancesystem/activity/AttendanceDataExportActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.lzf.attendancesystem.R;
 
@@ -19,6 +20,7 @@ public class AttendanceDataExportActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.UExport:
+                Toast.makeText(this, "未检测到U盘存储设备。", Toast.LENGTH_LONG).show();
                 break;
             case R.id.nativeView:
                 startActivity(new Intent(this, NativeViewActivity.class));
--- app/src/main/java/com/lzf/attendancesystem/activity/NativeViewActivity.java
+++ app/src/main/java/com/lzf/attendancesystem/activity/NativeViewActivity.java
@@ -1,7 +1,13 @@
 package com.lzf.attendancesystem.activity;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.lzf.attendancesystem.R;
@@ -10,14 +16,24 @@ import com.lzf.attendancesystem.bean.Attendance;
 import com.lzf.attendancesystem.bean.AttendanceDao;
 import com.lzf.attendancesystem.util.ReusableAdapter;
 
+import org.greenrobot.greendao.query.QueryBuilder;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class NativeViewActivity extends AppCompatActivity {
     private AttendanceDao attendanceDao = ZffApplication.getDaoSession(this).getAttendanceDao();
     private ReusableAdapter<Attendance> reusableAdapter;
-
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
 
+    private CheckBox staffIDCheck;
+    private CheckBox startTimeCheck;
+    private CheckBox endTimeCheck;
+    private EditText staffIDEdit;
+    private DatePicker startDatePicker;
+    private DatePicker endDatePicker;
+
+    @TargetApi(Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,5 +57,51 @@ public class NativeViewActivity extends AppCompatActivity {
             }
         };
         attendanceList.setAdapter(reusableAdapter);
+
+        staffIDCheck = findViewById(R.id.staffIDCheck);
+        startTimeCheck = findViewById(R.id.startTimeCheck);
+        endTimeCheck = findViewById(R.id.endTimeCheck);
+        staffIDEdit = findViewById(R.id.staffIDEdit);
+        startDatePicker = findViewById(R.id.startDatePicker);
+        endDatePicker = findViewById(R.id.endDatePicker);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.query:
+                try {
+                    String staffId = staffIDEdit.getText().toString().trim();
+                    long startDate = simpleDateFormat.parse(startDatePicker.getYear() + "年" + (startDatePicker.getMonth() + 1) + "月" + startDatePicker.getDayOfMonth() + "日" + " 00:00:00").getTime();
+                    long endDate = simpleDateFormat.parse(endDatePicker.getYear() + "年" + (endDatePicker.getMonth() + 1) + "月" + endDatePicker.getDayOfMonth() + "日" + " 23:59:59").getTime();
+                    QueryBuilder<Attendance> queryBuilder = attendanceDao.queryBuilder();
+                    if (staffIDCheck.isChecked()) {
+                        if (startTimeCheck.isChecked()) {
+                            if (endTimeCheck.isChecked()) {
+                                queryBuilder.where(AttendanceDao.Properties.StaffId.eq(staffId), queryBuilder.or(queryBuilder.and(AttendanceDao.Properties.SignInTime.ge(startDate), AttendanceDao.Properties.SignInTime.le(endDate)), queryBuilder.and(AttendanceDao.Properties.SignOutTime.ge(startDate), AttendanceDao.Properties.SignOutTime.le(endDate))));
+                            } else {
+                                queryBuilder.where(AttendanceDao.Properties.StaffId.eq(staffId), queryBuilder.or(AttendanceDao.Properties.SignInTime.ge(startDate), AttendanceDao.Properties.SignOutTime.ge(startDate)));
+                            }
+                        } else if (endTimeCheck.isChecked()) {
+                            queryBuilder.where(AttendanceDao.Properties.StaffId.eq(staffId), queryBuilder.or(AttendanceDao.Properties.SignInTime.le(endDate), AttendanceDao.Properties.SignOutTime.le(endDate)));
+                        } else {
+                            queryBuilder.where(AttendanceDao.Properties.StaffId.eq(staffId));
+                        }
+                    } else if (startTimeCheck.isChecked()) {
+                        if (endTimeCheck.isChecked()) {
+                            queryBuilder.where(queryBuilder.or(queryBuilder.and(AttendanceDao.Properties.SignInTime.ge(startDate), AttendanceDao.Properties.SignInTime.le(endDate)), queryBuilder.and(AttendanceDao.Properties.SignOutTime.ge(startDate), AttendanceDao.Properties.SignOutTime.le(endDate))));
+                        } else {
+                            queryBuilder.where(queryBuilder.or(AttendanceDao.Properties.SignInTime.ge(startDate), AttendanceDao.Properties.SignOutTime.ge(startDate)));
+                        }
+                    } else if (endTimeCheck.isChecked()) {
+                        queryBuilder.where(queryBuilder.or(AttendanceDao.Properties.SignInTime.le(endDate), AttendanceDao.Properties.SignOutTime.le(endDate)));
+                    }
+                    reusableAdapter.updateAll(queryBuilder.orderDesc(AttendanceDao.Properties.SignInTime).list());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
